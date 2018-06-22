cdcaf4bad7a44bb64b30a63a28890f854e943103
xdf29 <famxuandeng@gmail.com>
2018-06-22 10:54:39 +0800

    test base_reward = 0

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -98,10 +98,13 @@ namespace cryptonote {
     // {
       // base_reward = FINAL_SUBSIDY_PER_MINUTE*target_minutes;
     // }
-	if(base_reward < TEST_BASE_REWARD){
-		base_reward = TEST_BASE_REWARD;
-	}
 	
+	if(height == 0){
+		base_reward = 0;
+	}	
+	// if(base_reward < TEST_BASE_REWARD){
+		// base_reward = TEST_BASE_REWARD;
+	// }
 	
 	if(base_reward + already_generated_coins >= MONEY_SUPPLY){
 		base_reward = MONEY_SUPPLY - already_generated_coins*COIN;
